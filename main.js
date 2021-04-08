8b4bbececde8847ac86c7f524d4a96442036e37d
Majstruchka <maysstruk@ukr.net>
2021-04-08 22:55:44 +0300

    3

---
+++ main.js
@@ -0,0 +1,52 @@
+f = 20, m = 9 ;
+a = f / m ;
+console.log('Ускорение тела при силе F = ' , f , ', и массе m = ' , m ,' :');
+
+console.log('a = f / m = ', a);
+
+document.write('<p>**************</p>');
+
+document.write("<p>Ускорение тела при силе F = 20 , и массе m = 9 :</p>");
+document.write('<p>-------------------</p>');
+document.write('<p>a = 2,2222.</p>');
+document.write('<p>-------------------</p>');
+document.write('<p>end.</p>');
+
+
+console.log( 'Список студентов зарегестрировавшихся на вебинар:');
+age = '18 лет';
+name = 'Юрий';
+lastName = 'Иванов';
+number = "1.";
+
+fullName = number +  age + " " + name + " " + lastName;
+
+console.log(fullName);
+age = '16 лет ';
+name = 'Дмитрий';
+lastName = 'Кузьмин';
+number = "2.";
+
+fullName = number +  age + " " + name + " " + lastName;
+
+console.log(fullName);
+
+age = '19 лет ';
+name = 'Оксана';
+lastName = 'Петрова';
+number = "3.";
+
+fullName = number +  age + " " + name + " " + lastName;
+
+console.log(fullName);
+console.log('Средний возраст студента: [18 лет ]');
+
+document.write('<p**************</p>');
+document.write('<p>Список студентов зарегестрировавшихся на вебинар:</p>');
+document.write('<p>1. 18 лет Юрий Иванов</p>');
+document.write('<p>2. 16 лет Дмитрий Кузьмин</p>');
+document.write('<p>3. 19 лет Оксана Петрова</p>');
+document.write('-------------- ');
+document.write('Средний возраст студента: 18 лет ');
+document.write('-------------- ');
+document.write('<p>**************</p>');
